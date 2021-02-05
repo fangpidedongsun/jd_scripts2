04a0a89f3d01e4619632f037e93f4f48be1f9daf
fangpidedongsun <55946288+fangpidedongsun@users.noreply.github.com>
2021-02-05 08:34:40 +0800

    修复

--- getJDCookie.js
+++ getJDCookie.js
@@ -1,8 +1,8 @@
 /**
  * 扫码获取京东cookie，此方式得到的cookie有效期为90天(实际待测试)
- * @Author: lxk0301 https://github.com/lxk0301
+ * @Author: LXK9301 https://github.com/LXK9301
  * @Date: 2021-01-13 12:12:40
- * @Last Modified by: lxk0301
+ * @Last Modified by: LXK9301
  * @Last Modified time: 2021-01-13 12:22:54
  * Modify from FanchangWang https://github.com/FanchangWang
  */
@@ -61,6 +61,7 @@ function generateQrcode() {
           const url = 'https://plogin.m.jd.com/cgi-bin/m/tmauth?appid=300&client_type=m&token=' + token;
           qrcode.generate(url, {small: true}); // 输出二维码
           console.log("请打开 京东APP 扫码登录(二维码有效期为3分钟)");
+          console.log(`\n\n注：如扫描不到，请使用工具(例如在线二维码工具：https://cli.im)手动生成如下url二维码\n\n${url}\n\n`);
         }
       } catch (e) {
         $.logErr(e, resp)
