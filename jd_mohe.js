13bf7ecf3d5377fe8b1f21ca05b59aea466284f4
fangpidedongsun <55946288+fangpidedongsun@users.noreply.github.com>
2021-06-03 14:05:59 +0800

    Update jd_mohe.js

--- jd_mohe.js
+++ jd_mohe.js
@@ -1,8 +1,8 @@
 /*
 5G超级盲盒，可抽奖获得京豆，建议在凌晨0点时运行脚本，白天抽奖基本没有京豆，4小时运行一次收集热力值
 活动地址: https://isp5g.m.jd.com
-活动时间：2021-03-19到2021-04-30
-更新时间：2021-03-30 12:00
+活动时间：2021-06-2到2021-07-31
+更新时间：2021-06-3 12:00
 脚本兼容: QuantumultX, Surge,Loon, JSBox, Node.js
 =================================Quantumultx=========================
 [task_local]
@@ -43,10 +43,10 @@ $.shareId = [];
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  if ($.time('yyyy-MM-dd') === '2021-05-01') {
-    $.msg($.name, '活动已结束', `请禁用或删除脚本`);
-    return
-  }
+  console.log('5G超级盲盒，可抽奖获得京豆，建议在凌晨0点时运行脚本，白天抽奖基本没有京豆，4小时运行一次收集热力值\n' +
+      '活动地址: https://isp5g.m.jd.com\n' +
+      '活动时间：2021-06-2到2021-07-31\n' +
+      '更新时间：2021-06-3 12:00');
   await updateShareCodesCDN()
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
@@ -80,34 +80,13 @@ $.shareId = [];
     if ($.isNode()) await notify.sendNotify($.name, allMessage);
     $.msg($.name, '', allMessage, {"open-url": "https://isp5g.m.jd.com"})
   }
-  await $.http.get({url: `https://code.c-hiang.cn//api/v1/jd/mohe/read/20`, timeout: 10000}).then(async (resp) => {
-    if (resp.statusCode === 200) {
-      try {
-        let { body } = resp;
-        body = JSON.parse(body);
-        if (body && body['code'] === 200) {
-          $.body = body['data'];
-        }
-      } catch (e) {
-        console.log(`读取邀请码异常:${e}`)
-      }
-    }
-  }).catch((e) => console.log(`catch 读取邀请码异常:${e}`));
+  $.shareId = [...($.shareId || []), ...($.updatePkActivityIdRes || [])];
   for (let v = 0; v < cookiesArr.length; v++) {
     cookie = cookiesArr[v];
     $.index = v + 1;
     $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
     console.log(`\n\n自己账号内部互助`);
     for (let item of $.shareId) {
-      console.log(`账号 ${$.index} ${$.UserName} 开始给 ${item}进行助力`)
-      const res = await addShare(item);
-      if (res && res['code'] === 2005) {
-        console.log(`次数已用完，跳出助力`)
-        break
-      }
-    }
-    console.log(`\n\n如果有剩余助力机会则随机互助`);
-    for (let item of $.body || []) {
       console.log(`账号 ${$.index} ${$.UserName} 开始给 ${item}进行助力`)
       const res = await addShare(item);
       if (res && res['code'] === 2005) {
@@ -414,7 +393,7 @@ async function getAward() {
           console.log(`====抽奖结果====,${JSON.stringify(lotteryRes.data)}`);
           console.log(lotteryRes.data.name);
           console.log(lotteryRes.data.beanNum);
-          if ((lotteryRes.data['prizeId'] && lotteryRes.data['prizeId'] !== '9999') || lotteryRes.data.name !== '未中奖') {
+          if (lotteryRes.data['prizeId'] && (lotteryRes.data['type'] !== '99' && lotteryRes.data['type'] !== '3' && lotteryRes.data['type'] !== '8'  && lotteryRes.data['type'] !== '9')) {
             message += `抽奖获得：${lotteryRes.data.name}\n`;
           }
         } else if (lotteryRes.code === 4001) {
@@ -491,34 +470,16 @@ function shareUrl() {
     }
     $.get(options, async (err, resp, data) => {
       try {
-        // console.log('好友邀请码', data)
+        console.log('好友邀请码', data)
         data = JSON.parse(data);
         if (data['code'] === 5000) {
-          console.log(`重新运行一次脚本即可获取好友邀请码`)
+          console.log(`尝试多次运行脚本即可获取好友邀请码`)
         }
         // console.log('homeGoBrowse', data)
         if (data['code'] === 200) {
-          $.shareId.push(data['data']);
+          if (data['data']) $.shareId.push(data['data']);
           console.log(`\n【京东账号${$.index}（${$.nickName || $.UserName}）的${$.name}好友互助码】${data['data']}\n`);
           console.log(`此邀请码一天一变化，旧的不可用`)
-          $.http.get({url: `https://code.c-hiang.cn/autocommit/mohe/insert/${data['data']}`, timeout: 30000}).then((resp) => {
-            // console.log('resp', resp)
-            if (resp.statusCode === 200) {
-              try {
-                let { body } = resp;
-                body = JSON.parse(body);
-                if (body['code'] === 200) {
-                  console.log(`\n【京东账号${$.index}（${$.nickName || $.UserName}）的${$.name}好友互助码】${data['data']}提交成功\n`)
-                } else if (body['code'] === 400) {
-                  // console.log(`邀请码 【${data['data']}】 已存在\n`)
-                } else {
-                  console.log(`邀请码提交结果:${JSON.stringify(body)}\n`)
-                }
-              } catch (e) {
-                console.log(`邀请码提交异常:${e}`)
-              }
-            }
-          }).catch((e) => console.log(`catch 邀请码提交异常:${e}`));
         }
       } catch (e) {
         $.logErr(e, resp);
@@ -555,7 +516,7 @@ function updateShareCodesCDN(url = 'https://cdn.jsdelivr.net/gh/gitupdate/update
         } else {
           $.updatePkActivityIdRes = JSON.parse(data);
           if ($.updatePkActivityIdRes && $.updatePkActivityIdRes.length) {
-            $.shareId = $.updatePkActivityIdRes;
+            // $.shareId = $.updatePkActivityIdRes || [];
           }
         }
       } catch (e) {
